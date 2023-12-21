a56da58fd9494563d71434c325a673cc292dbe4f
Chris Richardson <Chris@ChrisRichardson.net>
2023-12-21 02:18:15 +0000

    Updates

---
+++ application/buildSrc/src/main/groovy/DockerBuildLocallyPlugin.groovy
@@ -0,0 +1,26 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
+
+class DockerBuildLocallyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+        project.apply(plugin: EnsureBuilderPlugin)
+
+        def args = ["docker", "buildx", "build", "--builder", EnsureBuilderPlugin.BUILDER_NAME, "--build-arg", "baseImageVersion=${project.ext.eventuateExamplesBaseImageVersion}",
+                "--platform", "linux/amd64,linux/arm64", "-t", "localhost:5002/${project.name.replace"-main", ""}:${project.ext.imageVersion}", "--output=type=image,push=true,registry.insecure=true",
+                "."]
+
+        def task = project.task("buildDockerImageLocally", type: Exec) {
+
+            workingDir(project.projectDir)
+
+            commandLine (args)
+
+            dependsOn(":registryComposeUp", ":${project.path}:assemble", "createBuilder")
+        }
+
+    }
+}
---
+++ application/customer-service/customer-service-main/src/componentTest/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/CustomerServiceComponentTest.java
@@ -0,0 +1,90 @@
+package io.eventuate.examples.tram.sagas.ordersandcustomers.customers;
+
+
+import io.eventuate.common.testcontainers.DatabaseContainerFactory;
+import io.eventuate.common.testcontainers.EventuateDatabaseContainer;
+import io.eventuate.common.testcontainers.EventuateZookeeperContainer;
+import io.eventuate.examples.springauthorizationserver.testcontainers.AuthorizationServerContainerForServiceContainers;
+import io.eventuate.messaging.kafka.testcontainers.EventuateKafkaCluster;
+import io.eventuate.messaging.kafka.testcontainers.EventuateKafkaContainer;
+import io.eventuate.testcontainers.service.ServiceContainer;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testcontainers.lifecycle.Startables;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.oauth2;
+
+public class CustomerServiceComponentTest {
+
+    public static EventuateKafkaCluster eventuateKafkaCluster = new EventuateKafkaCluster();
+
+    public static EventuateZookeeperContainer zookeeper = eventuateKafkaCluster.zookeeper;
+
+    public static EventuateKafkaContainer kafka = eventuateKafkaCluster.kafka.dependsOn(zookeeper);
+
+    public static EventuateDatabaseContainer<?> database =
+            DatabaseContainerFactory.makeVanillaDatabaseContainer()
+                    .withNetwork(eventuateKafkaCluster.network)
+                    .withNetworkAliases("customer-service-mysql")
+                    .withReuse(true);
+
+
+    public static AuthorizationServerContainerForServiceContainers authorizationServer = new AuthorizationServerContainerForServiceContainers()
+            .withNetwork(eventuateKafkaCluster.network)
+            .withNetworkAliases("authorization-server")
+            .withReuse(true);
+
+
+    public static ServiceContainer service =
+            new ServiceContainer("./Dockerfile", "../../gradle.properties")
+                    .withNetwork(eventuateKafkaCluster.network)
+                    .withDatabase(database)
+                    .withZookeeper(zookeeper)
+                    .withKafka(kafka)
+                    .dependsOn(kafka, database)
+                    .withEnv(authorizationServer.resourceServerEnv())
+                    .withLogConsumer(outputFrame -> System.out.print(outputFrame.getUtf8String()))
+                    .withReuse(false) // should rebuild
+            ;
+
+    @BeforeClass
+    public static void startContainers() {
+        Startables.deepStart(service, authorizationServer).join();
+    }
+
+    @Before
+    public void setup() {
+        RestAssured.port = service.getFirstMappedPort();
+        RestAssured.authentication = oauth2(authorizationServer.getJwt());
+    }
+
+    @Test
+    public void shouldStart() {
+        // HTTP
+        // Messaging
+    }
+
+
+    @Test
+    public void shouldGetCustomers() {
+        given()
+            .when()
+            .get("/customers")
+            .then()
+            .log().ifValidationFails()
+            .statusCode(200);
+    }
+
+    @Test
+    public void shouldGetActuatorHealth() {
+        given().
+            when()
+            .auth().none()
+            .get("/actuator/health")
+            .then()
+            .statusCode(200);
+    }
+}
